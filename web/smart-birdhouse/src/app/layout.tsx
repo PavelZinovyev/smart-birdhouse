30dd3164abc06607a859f43b8046a099e68b8d89
Pavel Zinoviev <frontenderpavel@gmail.com>
2026-02-22 16:29:23 +0300

    feat(ui): chart

--- web/smart-birdhouse/src/app/layout.tsx
+++ web/smart-birdhouse/src/app/layout.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import { Header } from '@/widgets';
 
 export const Layout = () => {
   return (
     <>
       <Header />
+      <main className="layout-main">
+        <Outlet />
+      </main>
     </>
   );
 };
