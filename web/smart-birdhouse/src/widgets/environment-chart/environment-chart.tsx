a9cb0a0ee513d6f31560f9647727a381321a538e
Pavel Zinoviev <pzinoviev@happynumbers.com>
2026-02-22 16:09:12 +0300

    feat(ux): header chart vite

---
+++ web/smart-birdhouse/src/widgets/environment-chart/environment-chart.tsx
@@ -0,0 +1,5 @@
+import { Chart } from '@/shared/ui/chart/chart';
+
+export const EnvironmentChart = () => {
+  return <Chart />;
+};
--- web/smart-birdhouse/vite.config.ts
+++ web/smart-birdhouse/vite.config.ts
@@ -1,13 +1,15 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
+import type { PluginOption } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr() as PluginOption],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
